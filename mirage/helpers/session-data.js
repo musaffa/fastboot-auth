191d70ceefa8cafdd0fcfbe646af927fe0a3e144
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2018-08-14 06:04:20 +0600

    Add 30 days of exp date for the failing case

--- mirage/helpers/session-data.js
+++ mirage/helpers/session-data.js
@@ -2,7 +2,8 @@ import jwt from 'jwt-simple';
 import { assign } from '@ember/polyfills';
 
 export default function(payload) {
-  let expiresIn = new Date().getTime() + 86400000; // one day from now
+  let days = 30;
+  let expiresIn = Math.floor(new Date().getTime() / 1000) + (days * 24 * 60 * 60);
   let payloadWithExp = assign({}, payload, { exp: expiresIn });
   let token = jwt.encode(payloadWithExp, 'secret');
   return { token };
