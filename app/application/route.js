c70b3f8081db0ddaf7a6997f0ed0c34d5ba40817
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2018-08-14 11:41:53 +0600

    Remove authentication route mixin

--- app/application/route.js
+++ app/application/route.js
@@ -1,6 +1,3 @@
 import Route from '@ember/routing/route';
-import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-const RouteWithMixin = Route.extend(ApplicationRouteMixin, {});
-
-export default class ApplicationRoute extends RouteWithMixin {}
+export default class ApplicationRoute extends Route {}
